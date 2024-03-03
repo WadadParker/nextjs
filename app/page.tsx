918cdd8f2beadff8961e1b3edcf5fa545b99ac07
Wadad Parker <wadadparker5@gmail.com>
2024-03-03 19:41:07 +0530

    feat: async server component data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.data;
+}
+
+// Async component to do async operations in top of a component
+export default async function Home() {
+  const userDetails = await getUserDetails();
+  // This is not possible in react, react does not support it
+  // But now it is the STANDARD way to fetch date in Netx.js
   return (
     <div>
       hi there
+      <aside>
+      <h1>{userDetails?.email}</h1>
+      {userDetails?.name}
+      </aside>
     </div>
   );
 }
